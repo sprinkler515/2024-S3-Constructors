822e75209a183f5e21b501dbfcb9b6730f1dc8ce
Tommy Chanthavisouk <wildcodeschool.sprinkler515@pm.me>
2024-04-15 14:03:44 +0200

    create main program

---
+++ Building.cs
@@ -0,0 +1,29 @@
+﻿using System.Dynamic;
+
+namespace _0414_S3_Constructors
+{
+    internal class Building
+    {
+        private readonly int _floors;
+        private readonly double _height;
+
+        public Building() { }
+
+        public Building(int floors): this()
+        {
+            _floors = floors;
+            _height = floors * 3;
+        }
+
+        public Building(int floors, double height) : this(floors)
+        {
+            _height = height;
+        }
+
+        public double GetFloorMaxSize() => _height / _floors;
+
+        public int GetFloorCount() => _floors;
+
+        public double GetSize() => _height;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace _0414_S3_Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors;
+            double height;
+            CultureInfo culture = new("en-US");
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            Console.Write("Enter a number of floors: ");
+            floors = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the desired size of the building: ");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            Building idealBuilding = new(floors);
+            Building customBuilding = new(floors, height);
+
+            Console.WriteLine("\nBuilding informations:");
+            DisplayBuilding(customBuilding);
+            Console.WriteLine("\nBuilding informations with size of floor (3m): ");
+            DisplayBuilding(idealBuilding);
+        }
+
+        static void DisplayBuilding(Building building)
+        {
+            Console.Write("Number of floors: ");
+            Console.WriteLine(building.GetFloorCount());
+            Console.Write("Size of building: ");
+            Console.WriteLine($"{building.GetSize():N2}m");
+            Console.Write("Floor size: ");
+            Console.WriteLine($"{building.GetFloorMaxSize():N2}m");
+        }
+    }
+}
